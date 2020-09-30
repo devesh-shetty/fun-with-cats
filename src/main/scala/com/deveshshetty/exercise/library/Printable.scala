b48fec9a544f1fa828b2c065c750c9750666717e
Devesh Shetty <devesh.shetty007@gmail.com>
2020-09-30 07:21:55 -0400

    Add Printable (close #1)

---
+++ src/main/scala/com/deveshshetty/exercise/library/Printable.scala
@@ -0,0 +1,18 @@
+package com.deveshshetty.exercise.library
+
+trait Printable[A] {
+  def format(value: A): String
+}
+
+object PrintableInstances {
+  implicit val stringPrintable: Printable[String] =
+    (value: String) => value
+
+  implicit val intPrintable: Printable[Int] =
+    (value: Int) => value.toString
+}
+
+object Printable {
+  def format[A](value: A)(implicit printable: Printable[A]): String = printable.format(value)
+  def print[A](value: A)(implicit printable: Printable[A]): Unit = println(format(value))
+}
